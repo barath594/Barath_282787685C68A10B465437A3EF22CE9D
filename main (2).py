6e199a52b7c8b14f1774cde23559a7fb5b52a70a
barath594 <144321618+barath594@users.noreply.github.com>
2023-09-13 19:39:58 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,16 +1,35 @@
-# 1.2 Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
+#2.1 Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.
 
 
-def isLeapYear(year):
-
-  if ((year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)):
-    return True
-  else:
-    return False
-
-
-year = 2020
-if isLeapYear(year):
-  print("{} is a leap Year".format(year))
-else:
-  print("{} is not a leap Year".format(year))
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
